641750d0951253471d72ccf96a9c802c8c7a154c
TheHumblePotato <yiyangl30@lakesideschool.org>
2026-07-18 16:49:06 -0700

    push

--- src/main/java/com/lmssmp/sidebar/SidebarContentBuilder.java
+++ src/main/java/com/lmssmp/sidebar/SidebarContentBuilder.java
@@ -132,10 +132,10 @@ public final class SidebarContentBuilder {
 				teamLine(player),
 				Component.empty(),
 				Component.literal("Leaderboard:"),
-				Component.literal("Red: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "team1")),
-				Component.literal("Yellow: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "team2")),
-				Component.literal("Green: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "team3")),
-				Component.literal("Blue: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "team4"))
+				Component.literal("Red: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "#team1")),
+				Component.literal("Yellow: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "#team2")),
+				Component.literal("Green: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "#team3")),
+				Component.literal("Blue: " + readNamedScore(player, SCORE_OBJECTIVE_NAME, "#team4"))
 		);
 
 		return new SidebarContent(TITLE, lines);
